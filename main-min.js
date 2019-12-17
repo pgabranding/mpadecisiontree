efcd6bcf2d64504374156a5cec13034810530158
jim <jim@huxleydigital.co.uk>
2019-12-17 15:04:47 +0000

    media page templating

--- main-min.js
+++ main-min.js
@@ -1,1 +1,1 @@
-alert("hi"),$(document).ready(function(){$(".answers a").css("border","10px solid red")});
+alert("hi"),$(document).ready(function(){$(".answers").css("border","10px solid red")});
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 alert('hi');
 
 $(document).ready(function () {
-    $(".answers a").css("border", "10px solid red");
+    $(".answers").css("border", "10px solid red");
 });
